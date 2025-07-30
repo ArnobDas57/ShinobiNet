ec5e4674ad6d8976d7ca713f86bb2bc319ed8ee9
Arnob Das <55168385+ArnobDas57@users.noreply.github.com>
2025-07-30 11:27:51 -0400

    feat: add Naruto spider for jutsu data extraction

---
+++ backend/jutsu_crawler.py
@@ -0,0 +1,32 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "narutospider"
+    start_urls = [
+        "https://naruto.fandom.com/wiki/Special:BrowseData/Jutsu?limit=250&offset=0&_cat=Jutsu",
+    ]
+
+    def parse(self, response):
+        for href in (
+            response.css("div.smw-columnlist-container")[0]
+            .css("a::attr(href)")
+            .extract()
+        ):
+            extracted_data = scrapy.Request(
+                "https://naruto.fandom.com" + href, callback=self.parse_jutsu
+            )
+
+            return extracted_data
+
+        next_page = response.css("mw-nextlink").get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
+
+    def parse_Jutsu(self, response):
+        jutsu_name = response.css("h1.page_header___title::text").extract()[0]
+        jutsu_name = jutsu_name.strip()
+
+        yield dict(
+            jutsu_name=jutsu_name,
+        )
